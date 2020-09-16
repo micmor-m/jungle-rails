e11de530d2560ee458e5d49dbac9f86df820444a
micmor-m <micmor.m@gmail.com>
2020-09-16 22:15:47 +0000

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  # resource :about, only: [:show]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -15,6 +16,10 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+  end
+
+  namespace :about do
+    root to: '/about#show'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
