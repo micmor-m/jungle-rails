8474a3b4bbff35c88f4495c2bb2ce7d37b88b6df
micmor-m <micmor.m@gmail.com>
2020-09-17 13:45:02 +0000

    update create action if the users controller to sanitize input from the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,22 @@ class UsersController < ApplicationController
   #new for rendering the signup form
   def new
   end
-  
+
   #create for receiving the form and creating a user with form's parameter
   def create
-  end 
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
 end
