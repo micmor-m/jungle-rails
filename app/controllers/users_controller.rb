1e5a5ca0a58725374a9c8ed787166159ebf14e1a
micmor-m <micmor.m@gmail.com>
2020-09-17 13:14:23 +0000

    add new and create action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  #new for rendering the signup form
+  def new
+  end
+  
+  #create for receiving the form and creating a user with form's parameter
+  def create
+  end 
 end
